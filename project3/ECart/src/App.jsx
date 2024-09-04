ac95c0abc6a1589b97899cf55127d70c994081ef
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-09-04 11:22:48 +0530

    change2

--- project3/ECart/src/App.jsx
+++ project3/ECart/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Product from './components/product'
-
-
+import { items } from './components/Data'
+import { BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import ProductDetail from './components/ProductDetail'
+import Cart from './components/Cart'
+import SearchItem from './components/SearchItem'
 const App = () => {
+  const [data, setdata] = useState([...items])
   return (
     <>
-   <Navbar/>
-   <Product/>
+    <Router>
+    <Navbar/>
+  <Routes>
+    <Route path="/" element={<Product items={data} />}/>
+    <Route path="/product/:id" element={<ProductDetail />}/>
+    <Route path="/search/:term" element={<SearchItem />}/>
+    <Route path="/cart" element={<Cart/>}/> 
+  </Routes>
+   
+
+    </Router>
+  
     </>
  
   )
--- project3/ECart/src/components/data.jsx
+++ project3/ECart/src/components/data.jsx
@@ -1,247 +1,85 @@
-export const Items=[ {
-    "brand": "",
-    "name": "",
-    "device": "AD681H",
-    "model": "Smartfren Andromax AD681H"
+export const items =  [
+  {
+    id: 1,
+    category: 'mobiles',
+    title: "Apple iPhone 14",
+    imgSrc: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg',
+    amazonLink: 'https://amzn.to/3PuckZp',
+    description: 'Apple iPhone 14 (128 GB) - Blue',
+    price: '89999',
   },
   {
-    "brand": "",
-    "name": "",
-    "device": "FJL21",
-    "model": "FJL21"
+    id: 5,
+    category: 'laptops',
+    title: "Xiaomi [Smartchoice] Notebookpro",
+    imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+    amazonLink: 'https://amzn.to/3EzeQaC',
+    description: 'Xiaomi [Smartchoice] Notebookpro ',
+    price: '49999',
   },
   {
-    "brand": "",
-    "name": "",
-    "device": "hws7721g",
-    "model": "MediaPad 7 Youth 2"
+    id: 7,
+    category: 'tablets',
+    title: "Xiaomi Pad 6",
+    imgSrc: 'https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg',
+    amazonLink: 'https://amzn.to/3ZeNRdY',
+    description: 'Xiaomi Pad 6| Qualcomm Snapdragon 870| ',
+    price: '29999',
   },
   {
-    "brand": "1&1",
-    "name": "1&1 Puck",
-    "device": "diw362_1u1",
-    "model": "DIW362P 1U1"
+    id: 2,
+    category: 'mobiles',
+    title: "Apple iPhone 11",
+    imgSrc: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg',
+    amazonLink: 'https://amzn.to/44MiYi6',
+    description: 'Apple iPhone 11 (64GB) - White',
+    price: '79999',
   },
   {
-    "brand": "1&1",
-    "name": "1&1 TV Box",
-    "device": "diw387_1u1",
-    "model": "DIW387 1U1"
+    id: 6,
+    category: 'tablets',
+    title: "Lenovo Tab P12 Pro AMOLED",
+    imgSrc: 'https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg',
+    amazonLink: 'https://amzn.to/3RabsdH',
+    description: 'Lenovo Tab P12 Pro AMOLED (12.6 inch (32 cm), 8 GB, 256 GB, Wi-fi Only), Storm Grey with Precision Pen 3',
+    price: '19999',
   },
   {
-    "brand": "1&1",
-    "name": "DIW387-1&1",
-    "device": "diw387_1u1",
-    "model": "DIW387 1U1"
+    id: 3,
+    category: 'mobiles',
+    title: "Apple iPhone 13",
+    imgSrc: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg',
+    amazonLink: 'https://amzn.to/3r6XJtv',
+    description: 'Apple iPhone 13 (128GB) - Blue',
+    price: '69999',
   },
   {
-    "brand": "10.or",
-    "name": "10or_G2",
-    "device": "G2",
-    "model": "10or G2"
+    id: 4,
+    category: 'laptops',
+    title: "Lenovo IdeaPad Slim 5 ",
+    imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+    amazonLink: 'https://amzn.to/44JsK4r',
+    description: 'Lenovo IdeaPad Slim 5 Intel Core i5 12450H 14" (35.5cm) ',
+    price: '29999',
   },
   {
-    "brand": "10.or",
-    "name": "D",
-    "device": "10or_D",
-    "model": "D"
+    id: 9,
+    category: 'tablets',
+    title: "Samsung Galaxy Tab A8",
+    imgSrc: 'https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1080_.jpg',
+    amazonLink: 'https://amzn.to/3Pdsh4F',
+    description: 'Samsung Galaxy Tab A8 26.69cm (10.5 inch) Display, RAM 4 GB, ROM 64 GB Expandable, Wi-Fi Tablet, Gray, (SM-X200NZAEINU)',
+    price: '49999',
   },
   {
-    "brand": "10.or",
-    "name": "E",
-    "device": "E",
-    "model": "E"
+    id: 8,
+    category: 'laptops',
+    title: "Apple MacBook Air Laptop",
+    imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+    amazonLink: 'https://amzn.to/3r9rvhm',
+    description: 'Apple MacBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display, ',
+    price: '89999',
   },
-  {
-    "brand": "10.or",
-    "name": "G",
-    "device": "G",
-    "model": "G"
-  },
-  {
-    "brand": "10.or",
-    "name": "G2",
-    "device": "G2",
-    "model": "G2"
-  },
-  {
-    "brand": "3222222 Satelital",
-    "name": "G706",
-    "device": "G706",
-    "model": "G706"
-  },
-  {
-    "brand": "3BBTV",
-    "name": "3BBTV",
-    "device": "S3000",
-    "model": "S3000-T0C08"
-  },
-  {
-    "brand": "3BBTV",
-    "name": "TBBTV01",
-    "device": "TBBTV01",
-    "model": "TBBTV01"
-  },
-  {
-    "brand": "3Go",
-    "name": "GT10K3IPS",
-    "device": "GT10K3IPS",
-    "model": "GT10K3IPS"
-  },
-  {
-    "brand": "3Go",
-    "name": "GT70053G",
-    "device": "GT70053G",
-    "model": "GT70053G"
-  },
-  {
-    "brand": "3Go",
-    "name": "GT7007EQC",
-    "device": "GT7007EQC",
-    "model": "GT7007EQC"
-  },
-  {
-    "brand": "3Q",
-    "name": "OC1020A",
-    "device": "OC1020A",
-    "model": "OC1020A"
-  },
-  {
-    "brand": "3Rtablet",
-    "name": "VT-7GA",
-    "device": "VT-7GA",
-    "model": "VT-7GA"
-  },
-  {
-    "brand": "3Rtablet",
-    "name": "VT-7GE",
-    "device": "VT-7GE",
-    "model": "VT-7GE"
-  },
-  {
-    "brand": "4good",
-    "name": "4GOOD Light B100",
-    "device": "4GOOD_Light_B100",
-    "model": "Light B100"
-  },
-  {
-    "brand": "4good",
-    "name": "A103",
-    "device": "4GOOD_Light_A103",
-    "model": "Light A103"
-  },
-  {
-    "brand": "5STAR",
-    "name": "5Star_Note_5",
-    "device": "5Star_Note_5",
-    "model": "5Star_Note_5"
-  },
-  {
-    "brand": "5STAR",
-    "name": "Note_1_Pro",
-    "device": "Note_1_Pro",
-    "model": "Note_1_Pro"
-  },
-  {
-    "brand": "7Eleven",
-    "name": "IN265",
-    "device": "IN265",
-    "model": "IN265"
-  },
-  {
-    "brand": "7mobile",
-    "name": "DRENA",
-    "device": "DRENA",
-    "model": "DRENA"
-  },
-  {
-    "brand": "7mobile",
-    "name": "KAMBA",
-    "device": "KAMBA",
-    "model": "KAMBA"
-  },
-  {
-    "brand": "7mobile",
-    "name": "Kamba 2",
-    "device": "7mobile_Kamba_2",
-    "model": "Kamba_2"
-  },
-  {
-    "brand": "7mobile",
-    "name": "SWEGUE",
-    "device": "SWEGUE",
-    "model": "SWEGUE"
-  },
-  {
-    "brand": "7mobile",
-    "name": "Swegue 2",
-    "device": "Swegue_2",
-    "model": "Swegue 2"
-  },
-  {
-    "brand": "9mobile",
-    "name": "DM_T901",
-    "device": "DM_T901",
-    "model": "DM_T901"
-  },
-  {
-    "brand": "A.O.I. ELECTRONICS FACTORY",
-    "name": "A.O.I.",
-    "device": "TR10CS1_11",
-    "model": "TR10CS1"
-  },
-  {
-    "brand": "A1",
-    "name": "A1 Alpha 20+",
-    "device": "P671F60",
-    "model": "A1 Alpha 20+"
-  },
-  {
-    "brand": "A1",
-    "name": "A1 Smart N9",
-    "device": "VFD720",
-    "model": "VFD 720"
-  },
-  {
-    "brand": "A1",
-    "name": "ADT-3",
-    "device": "adt3",
-    "model": "ADT-3"
-  },
-  {
-    "brand": "A1",
-    "name": "Ai-ES1059",
-    "device": "Ai-ES1059",
-    "model": "Ai-ES1059"
-  },
-  {
-    "brand": "AAUW",
-    "name": "T60Pro",
-    "device": "T60Pro",
-    "model": "T60Pro"
-  },
-  {
-    "brand": "AAUW",
-    "name": "T_50",
-    "device": "T_50",
-    "model": "T_50"
-  },
-  {
-    "brand": "ABCTECH",
-    "name": "PG11",
-    "device": "PG11",
-    "model": "PG11"
-  },
-  {
-    "brand": "ABCTECH",
-    "name": "X20",
-    "device": "X20",
-    "model": "X20"
-  },
-  {
-    "brand": "ABCTECH",
-    "name": "X20_Pro",
-    "device": "X20_Pro",
-    "model": "X20_Pro"
-  }]
-    
+
+
+]
